9a03de890ab089f73e67cfafdce11e987b1ed8d8
tkris <tkristian@my.wctc.edu>
2023-03-27 12:09:05 -0500

    [1.02]

--- Ticket.cs
+++ Ticket.cs
@@ -1,4 +1,4 @@
-public class Ticket
+public  class Ticket
 {
     public string ticketID {get; set; }
     public string sumry {get; set; }
@@ -7,8 +7,35 @@ public class Ticket
     public string sub {get; set;}
     public string assign {get; set;}
     public string watch {get; set;}
+    public List<String> severity {get; set;}
 
-    public string display() {
-        return $"Id: {ticketID}\nSummary: {sumry}\nStatus: {status}\nPriority: {priority}\nSubmitted by: {sub}\nAssigned by: {assign}\nPerson watching: {watch}";
+    public Ticket() 
+    {
+        severity = new List<string>();
+    }
+
+    public virtual string display() {
+        return $"Id: {ticketID}\nSummary: {sumry}\nStatus: {status}\nPriority: {priority}\nSubmitted by: {sub}\nAssigned by: {assign}\n Person watching: {watch}\nSeverity: {string.Join(", ",severity)}";
+    }
+}
+public class Enhancements : Ticket {
+  public string software {get; set;}
+  public string cost {get; set;}
+  public string reason {get; set;}
+  public string estimate {get; set;}
+
+
+  public override string display() {
+    return $"Id: {ticketID}\nSummary: {sumry}\nStatus: {status}\nPriority: {priority}\nSubmitted by: {sub}\nAssigned by: {assign}\nPerson Watching: {watch}\nSeverity: {string.Join(", ",severity)}\nSoftware: {software}\nCost: {cost}\nReason: {reason}\nEsstimate: {estimate}";
+  }
+  
+}
+public class Tasks : Ticket {
+    public string projectName {get; set;}
+    public string dueDate {get; set;}
+
+    public override string display()
+    {
+        return $"Id: {ticketID}\nSummary: {sumry}\nStatus: {status}\nPriority: {priority}\nSubmitted by: {sub}\nAssigned by: {assign}\nPerson Watching: {watch}\nProject Name: {projectName}\nDue date: {dueDate}";
     }
 }
